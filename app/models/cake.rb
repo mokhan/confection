9d853ec51401ef5c0c2914a88a69c10e5f853bf2
mo khan <mo@mokhan.ca>
2013-05-25 22:30:58 -0600

    create friendly url

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,4 +1,12 @@
 class Cake < ActiveRecord::Base
   attr_accessible :name, :photo, :photo_cache
   mount_uploader :photo, PhotoUploader
+
+  def slug
+    name.gsub(/[^a-z0-9]+/i, '-')
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
 end

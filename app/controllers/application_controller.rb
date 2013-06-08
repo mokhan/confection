ba987c3eb887537b7b720ca957849e92079d7d69
mo khan <mo@mokhan.ca>
2013-06-08 07:40:03 -0600

    load all categories in site nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :load_categories
+
+  def load_categories
+    @categories = Category.all
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :slug
   has_many :cakes
+
+  def to_param
+    slug
+  end
 end

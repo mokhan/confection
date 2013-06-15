c8a01e85536db992a38cb5cb4b6fec90d477c485
mo khan <mo@mokhan.ca>
2013-06-15 10:14:22 -0600

    add n+1 example

---
+++ samples/n_plus_q.sql
@@ -0,0 +1,5 @@
+SELECT  "cakes".* FROM "cakes"  LIMIT 10   
+SELECT  "users".* FROM "users"  WHERE "users"."id" = 1117 LIMIT 1   
+SELECT  "users".* FROM "users"  WHERE "users"."id" = 1972 LIMIT 1   
+SELECT  "users".* FROM "users"  WHERE "users"."id" = 1973 LIMIT 1   
+....
